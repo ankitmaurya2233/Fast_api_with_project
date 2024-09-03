802c84e031262c0c711fabe216fd2312d9d5b9da
Ankit Kumar Maurya <ankitmaurya2233@gamil.com>
2024-09-03 23:42:52 +0530

    path url

--- main.py
+++ main.py
@@ -5,4 +5,23 @@ app= FastAPI()
 
 @app.get('/')
 def Home():
-    return {"name":"Ankit"}
+    return {"Detail":"Ankit"}
+
+
+@app.get('/about')
+def about():
+    return {"Detail":"Ankit is a software engineer"}
+
+@app.get('/blog/unpublish')
+def unpublish():
+    return {"Unpublish":"unpublish blog"}
+
+# fatch blog with id
+@app.get('/blog/{id}')
+def blog(id:int):
+    return {"id": id, "title": "Sample Blog Post"}
+
+#fatch comment based on id
+@app.get("/blog/{id}/comment")
+def comment(id:int):
+    return {"id": id, "content": "This is a comment"}
